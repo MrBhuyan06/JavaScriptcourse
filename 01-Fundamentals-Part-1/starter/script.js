ecf2160c18d37a4db1cabe556823315ee8c25f66
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-11 18:22:35 +0530

    Added the Logical operator demo code

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -139,129 +139,161 @@
 
 // Template Literals
 
-const firstName = "Abhihske";
-const job = "coder";
-const age = 23;
-const year = 2037;
-const Abhishek =
-  "I' am " + firstName + ", A " + (year - age) + " year old" + job + "!";
-console.log(Abhishek);
+// const firstName = "Abhihske";
+// const job = "coder";
+// const age = 23;
+// const year = 2037;
+// const Abhishek =
+//   "I' am " + firstName + ", A " + (year - age) + " year old" + job + "!";
+// console.log(Abhishek);
 
-const abhiNew = `I' am ${firstName} , A ${year - age} year old ${job}!`;
-console.log(abhiNew);
+// const abhiNew = `I' am ${firstName} , A ${year - age} year old ${job}!`;
+// console.log(abhiNew);
 
-console.log(`Just A rgular String...`);
-console.log(
-  "string with \n\
-multiple line \n\
-"
-);
-console.log(`String
-multiline
-string
-`);
+// console.log(`Just A rgular String...`);
+// console.log(
+//   "string with \n\
+// multiple line \n\
+// "
+// );
+// console.log(`String
+// multiline
+// string
+// `);
 
-// If and else statement
+// // If and else statement
 
-const legalage = 12;
-const isOldEnough = legalage >= 18;
-if (isOldEnough) {
-  console.log("He is legal to Drive 🚘");
-} else {
-  const leftYear = 18 - legalage;
-  console.log(`
-  Opps Abhi is too Young . waith another ${leftYear} years`);
-}
+// const legalage = 12;
+// const isOldEnough = legalage >= 18;
+// if (isOldEnough) {
+//   console.log("He is legal to Drive 🚘");
+// } else {
+//   const leftYear = 18 - legalage;
+//   console.log(`
+//   Opps Abhi is too Young . waith another ${leftYear} years`);
+// }
 
-const birthYear = 20150;
-let century;
-if (birthYear <= 2000) {
-  century = 20;
-} else {
-  century = 21;
-}
-console.log(century);
+// const birthYear = 20150;
+// let century;
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+// console.log(century);
 
-// Type conversion and coercion
-const inputYear = "1991";
-console.log(Number(inputYear), inputYear);
-console.log(+inputYear + 18);
-// OR
-console.log(Number(inputYear) + 18);
-console.log(Number("1991"));
-console.log(String(23), 23);
-console.log(23 + "", 23);
+// // Type conversion and coercion
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(+inputYear + 18);
+// // OR
+// console.log(Number(inputYear) + 18);
+// console.log(Number("1991"));
+// console.log(String(23), 23);
+// console.log(23 + "", 23);
 
-// type coercion
-console.log("iam" + 23 + "years old");
-console.log("20" - "10" - 3);
-console.log("20" * "2");
-console.log("20" / "2");
+// // type coercion
+// console.log("iam" + 23 + "years old");
+// console.log("20" - "10" - 3);
+// console.log("20" * "2");
+// console.log("20" / "2");
 
-// guess the ouput
+// // guess the ouput
 
-let n = "1" + 1;
-n = n - 1;
-console.log(n); // Output 10
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n); // Output 10
 
-// truthy and falsy value
+// // truthy and falsy value
 
-// js there is 5 fasly value 0,'',undefine, null, NaN
+// // js there is 5 fasly value 0,'',undefine, null, NaN
 
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean(""));
-console.log(Boolean("abhi"));
-console.log(Boolean(NaN));
-console.log(Boolean(null));
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean(""));
+// console.log(Boolean("abhi"));
+// console.log(Boolean(NaN));
+// console.log(Boolean(null));
 
-const money = 100;
+// const money = 100;
 
-if (money) {
-  console.log("Don't spend it alll;");
-} else {
-  console.log("You Should get a job");
-}
+// if (money) {
+//   console.log("Don't spend it alll;");
+// } else {
+//   console.log("You Should get a job");
+// }
 
-let height = 0;
-if (height) {
-  console.log("Hey height is define");
-} else {
-  console.log("Height is undefine");
-}
+// let height = 0;
+// if (height) {
+//   console.log("Hey height is define");
+// } else {
+//   console.log("Height is undefine");
+// }
 
-// equality operator;
-// Strict operater
-// const ages = 18;
-// if (ages === 18) {
+// // equality operator;
+// // Strict operater
+// // const ages = 18;
+// // if (ages === 18) {
+// //   console.log(`You Just an adult`);
+// // } else {
+// //   console.log("You Are a under age");
+// // }
+
+// // Losely equil
+// const ages = "18";
+// if (ages == 18) {
 //   console.log(`You Just an adult`);
 // } else {
 //   console.log("You Are a under age");
 // }
 
-// Losely equil
-const ages = "18";
-if (ages == 18) {
-  console.log(`You Just an adult`);
+// let favNumber = Number(prompt("What's Your Favourite Number"));
+// console.log(favNumber);
+
+// if (favNumber === 23) {
+//   // '23' from prompt is type coersion and i compare to 23 number
+//   console.log("Cool! 23 is an amazing number");
+// } else if (favNumber === 7) {
+//   console.log("7 is also a Cool Number");
+// } else if (favNumber === 9) {
+//   console.log("9 is also a Cool Number");
+// } else {
+//   console.log("Number is no 7 and 23");
+// }
+
+// // different operator or not equal to operato
+// if (favNumber !== 23) {
+//   console.log("why not to try 23");
+// }
+
+// Boolean Logic
+// Logical Operator
+/**
+ * And &&
+ * or ||
+ * Not !12
+ */
+
+const hasDriverLicense = true;
+const hasGoodVision = true;
+console.log(hasDriverLicense && hasGoodVision);
+console.log(hasDriverLicense || hasGoodVision);
+console.log(!hasDriverLicense);
+console.log(!hasDriverLicense || hasGoodVision);
+
+const shouldDrive = hasDriverLicense && hasGoodVision;
+
+if (shouldDrive) {
+  console.log("Smriti Is Able to drive");
 } else {
-  console.log("You Are a under age");
+  console.log("Some else should is to drive");
 }
 
-let favNumber = Number(prompt("What's Your Favourite Number"));
-console.log(favNumber);
+const isTried = true;
+console.log(hasDriverLicense || hasGoodVision || isTried);
 
-if (favNumber === 23) {
-  // '23' from prompt is type coersion and i compare to 23 number
-  console.log("Cool! 23 is an amazing number");
-} else if (favNumber === 7) {
-  console.log("7 is also a Cool Number");
-} else if (favNumber === 9) {
-  console.log("9 is also a Cool Number");
+if (hasDriverLicense && hasGoodVision && isTried) {
+  console.log("Not good for driving");
 } else {
-  console.log("Number is no 7 and 23");
-}
-
-// different operator or not equal to operato
-if (favNumber !== 23) {
-  console.log("why not to try 23");
+  console.log("Good for drive");
 }
