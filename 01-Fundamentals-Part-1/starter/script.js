b35ed5e83ee50994116cd19e3026d02fda8972c9
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-13 09:46:22 +0530

    reviewing the code

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -183,20 +183,20 @@
 // console.log(century);
 
 // // Type conversion and coercion
-// const inputYear = "1991";
+const inputYear = "1991";
 // console.log(Number(inputYear), inputYear);
 // console.log(+inputYear + 18);
 // // OR
-// console.log(Number(inputYear) + 18);
+console.log(Number(inputYear) + 18);
 // console.log(Number("1991"));
 // console.log(String(23), 23);
-// console.log(23 + "", 23);
+console.log(23 + "", 23);
 
 // // type coercion
-// console.log("iam" + 23 + "years old");
-// console.log("20" - "10" - 3);
-// console.log("20" * "2");
-// console.log("20" / "2");
+console.log("iam" + 23 + "years old");
+console.log("20" - "10" - 3);
+console.log("20" * "2");
+console.log("20" / "2");
 
 // // guess the ouput
 
@@ -210,10 +210,11 @@
 
 // console.log(Boolean(0));
 // console.log(Boolean(undefined));
-// console.log(Boolean(""));
+// console.log(Boolean(" "));
 // console.log(Boolean("abhi"));
 // console.log(Boolean(NaN));
 // console.log(Boolean(null));
+console.log("HEy", "Hello" - undefined);
 
 // const money = 100;
 
