a48c490a8776388cf40b056f1a03f0d48ac9bd45
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-13 13:32:13 +0530

    Added the part 2 and strict mode

---
+++ 02-Fundamentals-part-2/starter/script.js
@@ -0,0 +1,13 @@
+// "use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+let interface = "HEllo";
+console.log(interface);
+if (passTest) {
+  hasDriversLicense = true;
+}
+if (hasDriversLicense) {
+  console.log("i can drive");
+}
