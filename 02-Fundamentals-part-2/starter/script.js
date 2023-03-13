0dc4842744a17884d85502d5f6471d648dd9b358
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-13 14:06:18 +0530

    Added the fn declaration and function expression

--- 02-Fundamentals-part-2/starter/script.js
+++ 02-Fundamentals-part-2/starter/script.js
@@ -22,6 +22,8 @@ logger();
 logger();
 // fn fname(Parameter)
 //fname(argument)
+
+// This method is function declartion
 function fruitProcessor(apples = 2, oranges) {
   console.log(apples, oranges);
   const juice = `juice With ${apples} apples and ${oranges} oranges`;
@@ -33,3 +35,19 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+//Function declartion
+function calcAge1(birthYear) {
+  const age = 2037 - birthYear;
+  return age;
+}
+const age1 = calcAge1(2000);
+console.log(age1);
+
+//Function Expression
+
+const calcAge2 = function (birthYeah) {
+  return 2037 - birthYeah;
+};
+const age2 = calcAge2(2000);
+console.log(age2);
