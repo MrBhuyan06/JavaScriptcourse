8c7b1c1bbe256167b921699c81989ca2b2a49c89
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-14 13:24:05 +0530

    Added Some BAsic Array Methods

--- 02-Fundamentals-part-2/starter/script.js
+++ 02-Fundamentals-part-2/starter/script.js
@@ -70,84 +70,118 @@
 
 // // Arrow function didnot get this keyWord
 
-function cutFruitPieces(fruit) {
-  return fruit * 4;
-}
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
 
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples);
-  const orangePieces = cutFruitPieces(oranges);
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
 
-  // console.log(apples, oranges);
-  const juice = `juice With ${applePieces} apples and ${orangePieces} oranges`;
-  return juice;
-}
+//   // console.log(apples, oranges);
+//   const juice = `juice With ${applePieces} apples and ${orangePieces} oranges`;
+//   return juice;
+// }
 
-console.log(fruitProcessor(2, 3));
+// console.log(fruitProcessor(2, 3));
 
-const calAge = function (birthYeah) {
-  return 2037 - birthYeah;
-};
+// const calAge = function (birthYeah) {
+//   return 2037 - birthYeah;
+// };
 
-const yearsUntilRetirement = function (birthyeah, firstName) {
-  const currentAge = calAge(birthyeah);
-  const retirement = 65 - currentAge;
+// const yearsUntilRetirement = function (birthyeah, firstName) {
+//   const currentAge = calAge(birthyeah);
+//   const retirement = 65 - currentAge;
 
-  if (retirement > 0) {
-    console.log(`${firstName}`);
-    return `${firstName} retires in ${retirement}`;
-  } else {
-    console.log(`${firstName}`);
-    return -1;
-  }
-};
+//   if (retirement > 0) {
+//     console.log(`${firstName}`);
+//     return `${firstName} retires in ${retirement}`;
+//   } else {
+//     console.log(`${firstName}`);
+//     return -1;
+//   }
+// };
 
-console.log(yearsUntilRetirement(1970, "Abhishek"));
+// console.log(yearsUntilRetirement(1970, "Abhishek"));
 
-// Array
+// // Array
 
-const friend1 = "Michael";
-const friend2 = "Steven";
-const friend3 = "Peter";
+// const friend1 = "Michael";
+// const friend2 = "Steven";
+// const friend3 = "Peter";
 
-//Literal Syntax
+// //Literal Syntax
+// const friends = ["Michael", "Steven", "Peter"];
+
+// console.log(friends);
+
+// const year = new Array(1991, 1984, 2008, 2020);
+// console.log(year);
+
+// // pointing to the paticular array element
+// console.log(friends[0]);
+// console.log(friends[2]);
+
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
+
+// console.log((friends[2] = "Jay"));
+// console.log(friends);
+// // friends = ["Bob", "Alice"];
+// const firstName = "Abhihske";
+// const jonas = ["FirstName", "Bhuyan", 2023 - 2000, "student", friends];
+// // As [Takes Expression u have a variable that produces data]
+// console.log(jonas);
+// console.log(jonas.length);
+
+// //Exercise
+// function calcAge1(birthYear) {
+//   const age = 2037 - birthYear;
+//   // Number- Array (NaN)
+//   return age;
+// }
+// const years = [1990, 1967, 2020, 2000, 2018];
+
+// const age1 = calcAge1(years[0]);
+// const age2 = calcAge1(years[1]);
+// const age3 = calcAge1(years[2]);
+// console.log(age1);
+// console.log(age2);
+// console.log(age3);
+// // We can Have function Call in The Array
+// const age = [calcAge1(years[0]), calcAge1(years[1])];
+// console.log(age);
+
+// Array Method
+
 const friends = ["Michael", "Steven", "Peter"];
+const newLenght = friends.push("jay"); // return the new Array Length
+console.log(friends);
+console.log(newLenght);
+// Attached to the end of the Array
 
+// Add Element to the being
+friends.unshift("John"); // return the Length of the new Array
 console.log(friends);
 
-const year = new Array(1991, 1984, 2008, 2020);
-console.log(year);
-
-// pointing to the paticular array element
-console.log(friends[0]);
-console.log(friends[2]);
-
-console.log(friends.length);
-console.log(friends[friends.length - 1]);
-
-console.log((friends[2] = "Jay"));
+// Remove Element form the last position
+console.log(`Before Pop ${friends}`);
+const popElement = friends.pop(); // return pop Element
+console.log(popElement);
 console.log(friends);
-// friends = ["Bob", "Alice"];
-const firstName = "Abhihske";
-const jonas = ["FirstName", "Bhuyan", 2023 - 2000, "student", friends];
-// As [Takes Expression u have a variable that produces data]
-console.log(jonas);
-console.log(jonas.length);
+console.log(`After Pop ${friends}`);
 
-//Exercise
-function calcAge1(birthYear) {
-  const age = 2037 - birthYear;
-  // Number- Array (NaN)
-  return age;
-}
-const years = [1990, 1967, 2020, 2000, 2018];
+//Remove Element form Starting postion
+console.log(`Before Shit ${friends}`);
+const shiftElement = friends.shift(); // return shift Element
+console.log(shiftElement);
+console.log(`After Shift ${friends}`);
 
-const age1 = calcAge1(years[0]);
-const age2 = calcAge1(years[1]);
-const age3 = calcAge1(years[2]);
-console.log(age1);
-console.log(age2);
-console.log(age3);
-// We can Have function Call in The Array
-const age = [calcAge1(years[0]), calcAge1(years[1])];
-console.log(age);
+//indexOf or postion of the element
+
+console.log(friends.indexOf("Steven"));
+console.log(friends.indexOf("bob")); // return -1
+
+// include moderm es6 methods return Boolean values
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
