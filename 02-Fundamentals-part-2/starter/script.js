38edfc3646a60d87132db659b127ba12b261b563
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-14 12:05:44 +0530

    Added ex for function calling a function

--- 02-Fundamentals-part-2/starter/script.js
+++ 02-Fundamentals-part-2/starter/script.js
@@ -1,71 +1,86 @@
-"use strict";
+// "use strict";
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-// let interface = "HEllo";
-// console.log(interface);
-if (passTest) {
-  hasDriversLicense = true;
+// // let interface = "HEllo";
+// // console.log(interface);
+// if (passTest) {
+//   hasDriversLicense = true;
+// }
+// if (hasDriversLicense) {
+//   console.log("i can drive");
+// }
+
+// //Function
+
+// function logger() {
+//   console.log("My name is Abhishek");
+// }
+// // Calling  / Running / Invoking function
+// logger();
+// logger();
+// // fn fname(Parameter)
+// //fname(argument)
+
+// // This method is function declartion
+// function fruitProcessor(apples = 2, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `juice With ${apples} apples and ${oranges} oranges`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// //Function declartion
+// function calcAge1(birthYear) {
+//   const age = 2037 - birthYear;
+//   return age;
+// }
+// const age1 = calcAge1(2000);
+// console.log(age1);
+
+// //Function Expression
+
+// const calcAge2 = function (birthYeah) {
+//   return 2037 - birthYeah;
+// };
+// console.log("Hello", calcAge2(2000));
+// const age2 = calcAge2(2000);
+// console.log(age2);
+
+// // Arrow Function
+// // 1. parameter and and return just line of code
+// const calsAge3 = (birthyeah) => 2037 - birthyeah;
+
+// const age3 = calsAge3(2000);
+// console.log(age1, age2, age3);
+
+// const yearsUntilRetirement = (birthyeah, firstName) => {
+//   const currentAge = 2037 - birthyeah;
+//   const retirement = 65 - currentAge;
+//   return `${firstName} retires in ${retirement}`;
+// };
+// console.log(yearsUntilRetirement(1991, "Abhihske"));
+// console.log(yearsUntilRetirement(2000, "Mr Bhuyan"));
+
+// // Arrow function didnot get this keyWord
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
 }
-if (hasDriversLicense) {
-  console.log("i can drive");
-}
 
-//Function
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
 
-function logger() {
-  console.log("My name is Abhishek");
-}
-// Calling  / Running / Invoking function
-logger();
-logger();
-// fn fname(Parameter)
-//fname(argument)
-
-// This method is function declartion
-function fruitProcessor(apples = 2, oranges) {
-  console.log(apples, oranges);
-  const juice = `juice With ${apples} apples and ${oranges} oranges`;
+  // console.log(apples, oranges);
+  const juice = `juice With ${applePieces} apples and ${orangePieces} oranges`;
   return juice;
 }
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
-
-//Function declartion
-function calcAge1(birthYear) {
-  const age = 2037 - birthYear;
-  return age;
-}
-const age1 = calcAge1(2000);
-console.log(age1);
-
-//Function Expression
-
-const calcAge2 = function (birthYeah) {
-  return 2037 - birthYeah;
-};
-console.log("Hello", calcAge2(2000));
-const age2 = calcAge2(2000);
-console.log(age2);
-
-// Arrow Function
-// 1. parameter and and return just line of code
-const calsAge3 = (birthyeah) => 2037 - birthyeah;
-
-const age3 = calsAge3(2000);
-console.log(age1, age2, age3);
-
-const yearsUntilRetirement = (birthyeah, firstName) => {
-  const currentAge = 2037 - birthyeah;
-  const retirement = 65 - currentAge;
-  return `${firstName} retires in ${retirement}`;
-};
-console.log(yearsUntilRetirement(1991, "Abhihske"));
-console.log(yearsUntilRetirement(2000, "Mr Bhuyan"));
-
-// Arrow function didnot get this keyWord
+console.log(fruitProcessor(2, 3));
