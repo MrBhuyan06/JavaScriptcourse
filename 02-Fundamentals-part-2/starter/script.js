26f5f06be848205549208d9c197d9dda716a38b5
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-14 13:10:06 +0530

    Added The Basic Of the Array

--- 02-Fundamentals-part-2/starter/script.js
+++ 02-Fundamentals-part-2/starter/script.js
@@ -103,3 +103,51 @@ const yearsUntilRetirement = function (birthyeah, firstName) {
 };
 
 console.log(yearsUntilRetirement(1970, "Abhishek"));
+
+// Array
+
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+//Literal Syntax
+const friends = ["Michael", "Steven", "Peter"];
+
+console.log(friends);
+
+const year = new Array(1991, 1984, 2008, 2020);
+console.log(year);
+
+// pointing to the paticular array element
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+console.log((friends[2] = "Jay"));
+console.log(friends);
+// friends = ["Bob", "Alice"];
+const firstName = "Abhihske";
+const jonas = ["FirstName", "Bhuyan", 2023 - 2000, "student", friends];
+// As [Takes Expression u have a variable that produces data]
+console.log(jonas);
+console.log(jonas.length);
+
+//Exercise
+function calcAge1(birthYear) {
+  const age = 2037 - birthYear;
+  // Number- Array (NaN)
+  return age;
+}
+const years = [1990, 1967, 2020, 2000, 2018];
+
+const age1 = calcAge1(years[0]);
+const age2 = calcAge1(years[1]);
+const age3 = calcAge1(years[2]);
+console.log(age1);
+console.log(age2);
+console.log(age3);
+// We can Have function Call in The Array
+const age = [calcAge1(years[0]), calcAge1(years[1])];
+console.log(age);
