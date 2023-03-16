5ba8e26078231f3e9d6e1354ab23989db473f1dd
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-16 12:05:08 +0530

    Added the init 03 file content

---
+++ 03-Developer-Skills/final/script copy.js
@@ -0,0 +1,5 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+
+const x = "900";
+const calAge = (birthyear) => 2023 - birthyear;
---
+++ 03-Developer-Skills/starter/script.js
@@ -0,0 +1,5 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+
+const x = "233";
+const age = (birth) => 2023 - birth;
