feb79f40e82b5b52fc6fa65a02c995d2dd4871f8
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-17 23:43:28 +0530

    Added the setup code of gusse the number project

--- 05-Guess My Number/script.js
+++ 05-Guess My Number/script.js
@@ -2,3 +2,10 @@
 
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'correct Number🥳';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+// get Data from Input Field
+
+console.log(document.querySelector('.guess').value);
