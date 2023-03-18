ae0cb195d321d24f034161d340893988773a362f
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-18 12:04:17 +0530

    Added full functionlity to guess Game

--- 05-Guess My Number/script.js
+++ 05-Guess My Number/script.js
@@ -11,12 +11,15 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+document.querySelector('.number').textContent = '?';
 let score = 20;
-
+let currentScore;
+let higherscore = 0;
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.quess').value);
+  // const guess = Number(document.querySelector('.quess').value);
+  const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
   //When No inPut
   if (!guess) {
@@ -27,6 +30,13 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🥳 Correct Number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    console.log(score);
+    // highscore = score;
+    if (score > higherscore) {
+      higherscore = score;
+      document.querySelector('.highscore').textContent = higherscore;
+    }
   }
   // Guess is To High
   else if (guess > secretNumber) {
@@ -52,4 +62,19 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+// Implementing Again Button Functionality
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log(secretNumber);
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.message').textContent = 'start guessing...';
+  document.querySelector('.highscore').textContent = 0;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.highscore').textContent = higherscore;
 });
