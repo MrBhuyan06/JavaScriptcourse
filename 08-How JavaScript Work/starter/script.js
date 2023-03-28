560d7e60a7f767373f64c5dd2e45957599bc78c8
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-28 13:51:05 +0530

    Added some example of block scope and function scope

--- 08-How JavaScript Work/starter/script.js
+++ 08-How JavaScript Work/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// Scope for block and funtion() level
+//  let and cost is block level level scope
+// - Here fullName is accessible to OutSide of block Scope
+// const age = 90;
+// if (age > 89) {
+//   var fullname = 'Abhishek';
+// }
+
+// console.log(fullname);
+// Function is also called as block level scope in strict mode
+
+// function a() {
+//   var fullname = 'Abhihshek';
+// }
+// console.log(fullname);
