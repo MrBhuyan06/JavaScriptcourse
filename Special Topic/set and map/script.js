f174f6f08081ac9cc09b72c1f1295ec0d0a99e47
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-04-30 19:25:56 +0530

    Added some exaple of Map Sets

--- Special Topic/set and map/script.js
+++ Special Topic/set and map/script.js
@@ -95,3 +95,52 @@ function displayObjectData({ ...objectRestuarent }) {
 const { name, ...all } = restaurant;
 console.log(name, all);
 displayObjectData({ ...restaurant });
+
+const arrayDp = [1, 2, 3, 3, 4];
+const sets = new Set(arrayDp);
+for (let ele of sets) {
+  console.log(ele);
+}
+const setOfString = new Set("Abhi");
+console.log(setOfString);
+console.log(sets);
+sets.add(12).add(14).add(15);
+console.log("hello", sets);
+console.log(sets.has(78));
+console.log(sets.delete(12));
+console.log(sets);
+console.log(sets.entries());
+console.log(sets.values());
+
+const maps = new Map();
+const arr = [1, 3];
+maps.set("abhi", "name");
+maps.set("abhi", "name");
+maps.set(arr, "test");
+console.log(maps.get(arr));
+console.log(maps);
+
+// convert an object to an object to an maps
+const details = {
+  name: "Abhishek",
+  age: 12,
+  address: "sundarpada",
+};
+console.log(typeof details.name);
+console.log(Object.entries(details));
+const convertMap = new Map(Object.entries(details));
+console.log(convertMap);
+console.log(...convertMap);
+
+// covert map to array
+const somearr = [
+  [3, 4],
+  [4, 5],
+];
+const mapToArray = new Map(somearr);
+console.log(mapToArray);
+const mapToarrayVariable = [[...mapToArray]];
+console.log(mapToarrayVariable);
+// const newSomeArray = [1, 2, 3, ...somearr];
+console.log(...somearr);
+// console.log(newSomeArray);
