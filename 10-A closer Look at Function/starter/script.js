cf14d387241db45c5d87755fc457fc114613e1f3
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-05-02 22:58:24 +0530

    Added the call back and higher order function

--- 10-A closer Look at Function/starter/script.js
+++ 10-A closer Look at Function/starter/script.js
@@ -22,34 +22,54 @@
 // createBooking('LH1234', undefined, 800);
 // console.log(bookings);
 
-const flight = 'LH234';
-const abhi = {
-  name: 'Abhihske Bhuyna',
-  passport: 234567,
-};
+// const flight = 'LH234';
+// const abhi = {
+//   name: 'Abhihske Bhuyna',
+//   passport: 234567,
+// };
 
-const chechIn = function (flightNum, passenger) {
-  flightNum = 'LH9999';
-  passenger.name = 'Mr.' + passenger.name;
-  if (passenger.passport === 234567) {
-    alert('checKIN');
-  } else {
-    alert('wrong passport');
-  }
-};
-chechIn(flight, abhi);
-console.log(flight);
-console.log(abhi);
+// const chechIn = function (flightNum, passenger) {
+//   flightNum = 'LH9999';
+//   passenger.name = 'Mr.' + passenger.name;
+//   if (passenger.passport === 234567) {
+//     alert('checKIN');
+//   } else {
+//     alert('wrong passport');
+//   }
+// };
+// chechIn(flight, abhi);
+// console.log(flight);
+// console.log(abhi);
 
-// is same as doing
-const flightNum = flight;
-const passenger = abhi;
+// // is same as doing
+// const flightNum = flight;
+// const passenger = abhi;
 
-const newPasswort = function (person) {
-  person.passport = Math.trunc(Math.random() * 10000000000);
-};
-newPasswort(abhi);
-chechIn(flight, abhi);
+// const newPasswort = function (person) {
+//   person.passport = Math.trunc(Math.random() * 10000000000);
+// };
+// newPasswort(abhi);
+// chechIn(flight, abhi);
 
 //pass by value and passing in refrence
 //javascript is not has passing by refrence it pasing by value but it look like passing by refrence
+
+//first class and higher order functions
+//create a own higher order function
+const oneWord = function (str) {
+  return str.replaceAll(' ', '').toLowerCase();
+};
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+//HigherOther function
+const transformer = function (str, fn) {
+  console.log(`Transformer: ${fn(str)}`);
+  console.log(`Transformer: ${str}`);
+  console.log(`Transformer by ${fn.name}`);
+};
+
+transformer('JavaScript is the best', upperFirstWord);
+transformer('JavaScript is the best', oneWord);
