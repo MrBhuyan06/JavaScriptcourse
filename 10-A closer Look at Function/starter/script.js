4f63b40b0133d7e5d934c2d1b327021a22476e84
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-05-10 15:01:17 +0530

    push code for applay and call method

--- 10-A closer Look at Function/starter/script.js
+++ 10-A closer Look at Function/starter/script.js
@@ -54,83 +54,137 @@
 //pass by value and passing in refrence
 //javascript is not has passing by refrence it pasing by value but it look like passing by refrence
 
-let myName = 'Abhishek';
-const changeName = function (myName) {
-  myName = 'AbhihskeBhuyna';
-  console.log(myName);
+// let myName = 'Abhishek';
+// const changeName = function (myName) {
+//   myName = 'AbhihskeBhuyna';
+//   console.log(myName);
+// };
+// console.log(myName);
+// changeName(myName);
+// console.log('Hemmlo', myName);
+
+// //first class and higher order functions
+// //create a own higher order function
+// const oneWord = function (str) {
+//   return str.replaceAll(' ', '').toLowerCase();
+// };
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   console.log(others);
+//   console.log('hello', ...others);
+//   console.log([first.toUpperCase(), ...others].join(' '));
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
+
+// //HigherOther function
+// const transformer = function (str, fn) {
+//   console.log(`Transformer: ${fn(str)}`);
+//   console.log(`Transformer: ${str}`);
+//   console.log(`Transformer by ${fn.name}`);
+// };
+
+// transformer('JavaScript is the best', upperFirstWord);
+// transformer('JavaScript is the best', oneWord);
+
+// const greets = function () {
+//   return 'Namaster';
+// };
+
+// const welcomeMessage = name => {
+//   let greet = greets();
+//   return name + greet;
+// };
+// console.log(welcomeMessage('abhi'));
+
+// // Questin demostrated the higher order function
+// // made a fucntion which take a str as input produce the single world
+// const oneWord2 = function (str) {
+//   return str.replaceAll(' ', '').toLowerCase();
+// };
+
+// //made a function which take a str as input and transform the str
+// //as Abhishek Bhuyna
+// // outout ABHISHEK bhuyna
+// const changeStr = function (str) {
+//   const [first, ...second] = str.split(' ');
+//   return [first.toUpperCase(), ...second].join(' ');
+// };
+
+// const tranformer = function (str, fn) {
+//   console.log('str before Applay Method');
+//   console.log(
+//     `Str and this is done by functionName ${fn.name} aftre Applay Method ${fn(
+//       str
+//     )}`
+//   );
+// };
+
+// tranformer('Abhi bhuyna', oneWord2);
+// tranformer('Abhi bhuyna', changeStr);
+// tranformer('Abhi bhuyna', changeStr);
+
+// // Function return anthore function
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+// const GreetHey = greet('Hey');
+// console.log(GreetHey);
+// GreetHey('Abhihske');
+// GreetHey('Smriti');
+
+// const greet = greeting => {
+//   return name => {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+// const greetHey = greet('hey');
+// greetHey('Abhi');
+
+// Call and Applay method
+const airIndia = {
+  airline: 'airIndiaJet',
+  alcode: 'AIT',
+  Booking: [],
+  book(seatNum, passengerName) {
+    console.log(
+      `${passengerName} book a set on ${this.airline} flight ${this.alcode}`
+    );
+    this.Booking.push({
+      flight: `${this.airline}${this.alcode}`,
+      name: passengerName,
+    });
+  },
 };
-console.log(myName);
-changeName(myName);
-console.log('Hemmlo', myName);
-
-//first class and higher order functions
-//create a own higher order function
-const oneWord = function (str) {
-  return str.replaceAll(' ', '').toLowerCase();
-};
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  console.log(others);
-  console.log('hello', ...others);
-  console.log([first.toUpperCase(), ...others].join(' '));
-  return [first.toUpperCase(), ...others].join(' ');
+const airAsisa = {
+  airline: 'airAsisa',
+  alcode: 'AS',
+  Booking: [],
 };
 
-//HigherOther function
-const transformer = function (str, fn) {
-  console.log(`Transformer: ${fn(str)}`);
-  console.log(`Transformer: ${str}`);
-  console.log(`Transformer by ${fn.name}`);
+airIndia.book(212, 'Abhishek Bhuyan');
+
+// Storing the method refrence into individual Variabl
+const book = airIndia.book;
+// console.log(book);
+
+//As we know this variable is dyanamic this value is basically depends how the function is called as here this is just a normal function call so in normal function call this is belong to the undefine as strict mode
+// book(2121, 'Smriti Pradhan');
+// So what we can do for this
+
+book.call(airAsisa, 212, 'Smriti Pradhan');
+
+const airUk = {
+  airline: 'airUk',
+  alcode: 'AS',
+  Booking: [],
 };
 
-transformer('JavaScript is the best', upperFirstWord);
-transformer('JavaScript is the best', oneWord);
+const callArgument = [232, 'Satabdi Gadhai'];
+book.apply(airUk, callArgument);
+//  Or the moderm way to do so is very easy
+book.call(airAsisa, ...callArgument);
 
-const greets = function () {
-  return 'Namaster';
-};
-
-const welcomeMessage = name => {
-  let greet = greets();
-  return name + greet;
-};
-console.log(welcomeMessage('abhi'));
-
-// Questin demostrated the higher order function
-// made a fucntion which take a str as input produce the single world
-const oneWord2 = function (str) {
-  return str.replaceAll(' ', '').toLowerCase();
-};
-
-//made a function which take a str as input and transform the str
-//as Abhishek Bhuyna
-// outout ABHISHEK bhuyna
-const changeStr = function (str) {
-  const [first, ...second] = str.split(' ');
-  return [first.toUpperCase(), ...second].join(' ');
-};
-
-const tranformer = function (str, fn) {
-  console.log('str before Applay Method');
-  console.log(
-    `Str and this is done by functionName ${fn.name} aftre Applay Method ${fn(
-      str
-    )}`
-  );
-};
-
-tranformer('Abhi bhuyna', oneWord2);
-tranformer('Abhi bhuyna', changeStr);
-tranformer('Abhi bhuyna', changeStr);
-
-// Function return anthore function
-
-const greet = function (greeting) {
-  return function (name) {
-    console.log(`${greeting} ${name}`);
-  };
-};
-const GreetHey = greet('Hey');
-console.log(GreetHey);
-GreetHey('Abhihske');
-GreetHey('Smriti');
+// Build a call And apply menthod example using a gym example
