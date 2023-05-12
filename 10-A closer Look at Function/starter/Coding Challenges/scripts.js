75c6da881dd7aca2e0b1587e041077828fc3962d
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-05-12 14:19:15 +0530

    Added closure  code challenge 2

--- 10-A closer Look at Function/starter/Coding Challenges/scripts.js
+++ 10-A closer Look at Function/starter/Coding Challenges/scripts.js
@@ -34,47 +34,47 @@
 // § Data 1: [5, 2, 3]
 // § Data 2: [1, 5, 3, 9, 6, 1]
 
-const poll = {
-  question: 'What is your favourite programming language?',
-  option: ['0: JavaScript', '1: Python', '2: Rust', '3: c++'],
-  answers: new Array(4).fill(0),
-  registerNewAnswer() {
-    const userAns = Number(
-      prompt(
-        `${this.question}\n ${this.option.join('\n')}}\n(Write option number`
-      )
-    );
-    console.log(userAns);
-    console.log(this.answers[userAns]);
-    // userAns <= 3
-    //   ? (this.answers[userAns] += 1)
-    //   : console.log('please provide valid option');
-    // console.log(this.answers[userAns]);
-    // this.displayResults();
+// const poll = {
+//   question: 'What is your favourite programming language?',
+//   option: ['0: JavaScript', '1: Python', '2: Rust', '3: c++'],
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     const userAns = Number(
+//       prompt(
+//         `${this.question}\n ${this.option.join('\n')}}\n(Write option number`
+//       )
+//     );
+//     console.log(userAns);
+//     console.log(this.answers[userAns]);
+//     // userAns <= 3
+//     //   ? (this.answers[userAns] += 1)
+//     //   : console.log('please provide valid option');
+//     // console.log(this.answers[userAns]);
+//     // this.displayResults();
 
-    // more precise as per question
-    typeof answer === 'number' &&
-      answer < this.answers.length &&
-      this.answers[answer]++;
-    this.displayResults();
-    this.displayResults('string');
-  },
-  displayResults(type = 'array') {
-    if (typeof type === 'array') {
-      console.log(this.answers);
-    } else {
-      console.log(`poll result are ${this.answers.join(', ')}`);
-    }
-  },
-};
-// poll.registerNewAnswer();
-// const pollclick = poll.registerNewAnswer;
-document
-  .getElementById('btn')
-  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+//     // more precise as per question
+//     typeof answer === 'number' &&
+//       answer < this.answers.length &&
+//       this.answers[answer]++;
+//     this.displayResults();
+//     this.displayResults('string');
+//   },
+//   displayResults(type = 'array') {
+//     if (typeof type === 'array') {
+//       console.log(this.answers);
+//     } else {
+//       console.log(`poll result are ${this.answers.join(', ')}`);
+//     }
+//   },
+// };
+// // poll.registerNewAnswer();
+// // const pollclick = poll.registerNewAnswer;
+// document
+//   .getElementById('btn')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-poll.displayResults.call(poll, 'string');
-poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+// poll.displayResults.call(poll, 'string');
+// poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
 // poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
 // poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 
@@ -118,3 +118,29 @@ poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
 // poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
 // poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
 // poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+
+// Coding Challenge #2
+// This is more of a thinking challenge than a coding challenge �
+// Your tasks:
+// 1. Take the IIFE below and at the end of the function, attach an event listener that
+// changes the color of the selected h1 element ('header') to blue, each time
+// the body element is clicked. Do not select the h1 element again!
+// 2. And now explain to yourself (or someone around you) why this worked! Take all
+// the time you need. Think about when exactly the callback function is executed,
+// and what that means for the variables involved in this example.
+
+// document.getElementsByTagName('body').addEventListener(
+//   'click',
+//   (function () {
+//     const header = document.querySelector('h1');
+//     header.style.color = 'red';
+//   })()
+// );
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'green';
+  });
+})();
