4210d9a9529c30298b2cd665a49aef8acb6afa32
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-07-15 11:59:40 +0530

    class methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -353,3 +353,41 @@ const jay = Object.create(StudentProto);
 jay.init('Abhi', 2000, 'cs');
 jay.calcAge();
 jay.introduces();
+
+//class examples
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.local = navigator.language;
+    console.log(`Thanks for Opening an account ${owner}`);
+  }
+  //interface of an object or an api 0r public interface of an object
+  deposit(val) {
+    this.movements.push(val);
+  }
+  withdraw(val) {
+    this.deposit(-val);
+  }
+  approveLoan(val) {
+    return true;
+  }
+  requestLoan(val) {
+    if (this.approveLoan) {
+      this.deposit(val);
+      console.log('Loan Approved');
+    }
+  }
+}
+const acc1 = new Account('Abhi', 'IND', 1111);
+// console.log(acc1);
+// acc1.movements.push(240);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(100);
+acc1.approveLoan(100);
+console.log(acc1);
+console.log(acc1.pin);
