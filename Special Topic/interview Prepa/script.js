e2ae7abd7ea40ba50d6913ecd543f1cfd29cad22
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-08-16 16:44:01 +0530

    added the var and let and cost scope

--- Special Topic/interview Prepa/script.js
+++ Special Topic/interview Prepa/script.js
@@ -1,5 +1,21 @@
-printName();
-function printName() {
-  console.log(names);
+// printName();
+// function printName() {
+//   console.log(names);
+// }
+// let names = "Abhihske";
+
+/* {
+  let names = "abhishek";
+  var age = "23";
+  const add = "bbsr";
 }
-let names = "Abhihske";
+console.log(age);
+console.log(names);
+console.log(add); */
+// console.log("abh");
+
+// function scopeVar() {
+//   var names = "abhishek";
+// }
+
+// console.log(names);
