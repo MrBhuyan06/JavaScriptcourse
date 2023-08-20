a1d4f2913580d8e7ceeb42568fb8a51737d1fba0
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-08-20 21:43:49 +0530

    using one loop

--- Special Topic/interview Prepa/DSA/ARRAY/storeDuplicate.js
+++ Special Topic/interview Prepa/DSA/ARRAY/storeDuplicate.js
@@ -1,6 +1,15 @@
 //store Duplicate ele in an array
 //[1,2,3,4,2]
 //[2]
+// let arr = [1, 2, 3, 4, 2];
+// let duplicate = arr.filter((ele, index) => arr.indexOf(ele) != index);
+// console.log(duplicate);
+
+let dup = [];
 let arr = [1, 2, 3, 4, 2];
-let duplicate = arr.filter((ele, index) => arr.indexOf(ele) != index);
-console.log(duplicate);
+for (let i = 0; i < arr.length; i++) {
+  if (arr.indexOf(arr[i]) != i) {
+    dup.push(arr[i]);
+  }
+}
+console.log(dup);
