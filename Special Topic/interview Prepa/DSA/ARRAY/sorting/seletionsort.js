9f1176f6c4f49a10f5c5b87cea9ed32935c9b7ce
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-08-23 23:02:32 +0530

    added selection sort

---
+++ Special Topic/interview Prepa/DSA/ARRAY/sorting/seletionsort.js
@@ -0,0 +1,20 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    //store the minvalue and minindex
+    let minIndex = i;
+    let min = arr[i];
+    for (let j = 1; j < arr.length; j++) {
+      if (arr[j] < min) {
+        min = arr[j];
+        minIndex = j;
+      }
+    }
+    if (minIndex != i) {
+      arr[minIndex] = arr[i];
+      arr[i] = min;
+    }
+  }
+  console.log(arr);
+}
+
+bubbleSort([3, 1, 2]);
