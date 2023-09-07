a2290d94e73086298f2eaa252b51c64c7e490427
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-07 22:15:15 +0530

    added the race methods

--- Special Topic/Promises/promisesmethods.js
+++ Special Topic/Promises/promisesmethods.js
@@ -45,22 +45,25 @@
 
 //define promise in different way
 
-let p1 = new Promise((res, rej) => {
-  if (true) {
-    res("p1");
-  }
-});
-let p2 = new Promise((res, rej) => {
-  if (true) {
-    res("p2");
-  }
-});
-let p3 = new Promise((res, rej) => {
-  if (true) {
-    res("p2");
-  }
-});
-// let res;
+// let p1 = new Promise((res, rej) => {
+//   if (true) {
+//     res("p1");
+//   }
+// });
+// let p2 = new Promise((res, rej) => {
+//   if (true) {
+//     res("p2");
+//   }
+// });
+// let p3 = new Promise((res, rej) => {
+//   if (true) {
+//     res("p2");
+//   }
+// });
+// Promise.all([p1, p2, p3]).then((data) => {
+//   console.log(data);
+// });
+// // let res;
 
 // async function resPromise() {
 //   console.log("came");
@@ -71,3 +74,67 @@ let p3 = new Promise((res, rej) => {
 // }
 // resPromise();
 // console.log(res);
+
+// 2.allsettled
+/**
+ * ! this is return all the settled promise fullfiled and rejected
+ * ! each res will covert object
+ *  0
+: 
+{status: 'fulfilled', value: 'p1'}
+1
+: 
+{status: 'fulfilled', value: 'p2'}
+2
+: 
+{status: 'fulfilled', reason: 'p2'}// rejected responses
+length
+: 
+3
+ *
+ *
+ *
+ */
+
+//async behaviour Promise
+
+let p1 = new Promise((res, rej) => {
+  let random = Math.trunc(Math.random() * 6 + 1);
+  setTimeout(() => {
+    if (true) {
+      res("p1");
+    }
+  }, random * 1000);
+});
+let p2 = new Promise((res, rej) => {
+  let random = Math.trunc(Math.random() * 6 + 1);
+  setTimeout(() => {
+    if (true) {
+      rej("p2 error");
+    }
+  }, random * 1000);
+});
+let p3 = new Promise((res, rej) => {
+  let random = Math.trunc(Math.random() * 6 + 1);
+  setTimeout(() => {
+    if (true) {
+      res("p3");
+    }
+  }, random * 1000);
+});
+
+/***
+ * !race
+ * * As the name say which promise is settled fast it return that sattled promise data
+ *
+ *
+ *
+ */
+
+Promise.race([p1, p2, p3])
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
