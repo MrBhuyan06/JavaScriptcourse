63436e726fe6ffc333e7b675d12e0f29db58acad
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-08 16:45:55 +0530

    added the sum in all approach

---
+++ Special Topic/interview Prepa/DSA/ARRAY/sumofallnumber.js
@@ -0,0 +1,40 @@
+// sum of all number in a array
+let arr = [1, 2, 3, 4, 5];
+
+//using for loop
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   sum = sum + arr[i];
+// }
+// console.log(sum);
+//using the reducer method
+// let sum = arr.reduce((sum, curr) => {
+//   return sum + curr;
+// }, 0);
+// console.log(sum);
+//for Each
+// let max = 0;
+// arr.forEach((ele) => {
+//   max = max + ele;
+// });
+// console.log(max);
+// using for of
+// let sum = 0;
+// for (ele of arr) {
+//   sum = sum + ele;
+// }
+// console.log(sum);
+//using for in
+// let sum = 0;
+// for (index in arr) {
+//   sum = sum + arr[index];
+// }
+// console.log(sum);
+//while loop
+// let i = 0;
+// let sum = 0;
+// while (i < arr.length) {
+//   sum = sum + arr[i];
+//   i++;
+// }
+// console.log(sum);
