896ad09a4020bc44fb24ffa12e31468f314470f2
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-08 16:24:10 +0530

    added the find the largest number using 6 approach

--- Special Topic/interview Prepa/DSA/ARRAY/findlargest.js
+++ Special Topic/interview Prepa/DSA/ARRAY/findlargest.js
@@ -2,10 +2,39 @@
 let arr = [21, 12, 34, 11, 32, 45, 1, 5, 28];
 
 //make the first index as the largest
-let largest = arr[0];
-for (let i = 1; i < arr.length; i++) {
-  if (arr[i] > largest) {
-    largest = arr[i];
-  }
-}
-console.log(`The largest number is ${largest}`);
+// let largest = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] > largest) {
+//     largest = arr[i];
+//   }
+// }
+// console.log(`The largest number is ${largest}`);
+
+// we have multiple approach to slove this problem
+// 1.Using Math.max
+// let largestNum = Math.max(...arr);
+// console.log(largestNum);
+//2.using the reducers
+// const largestNo = arr.reduce((max, curr) => (max > curr ? max : curr), arr[0]);
+// console.log(largestNo);
+// 3.using sort methods
+// const largestNumber = arr.sort((a, b) => b - a)[0];
+// console.log(largestNumber);
+// 4.Math.max using applay method
+// const largestNumber = Math.max.apply(null, arr);
+// console.log(largestNumber);
+// 5.forEach loop
+// let maxNumber = arr[0];
+// arr.forEach((num) => {
+//   if (num > maxNumber) {
+//     maxNumber = num;
+//   }
+// });
+//6 for loop
+// let maxNumber = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] > maxNumber) {
+//     maxNumber = arr[i];
+//   }
+// }
+// console.log(maxNumber);
