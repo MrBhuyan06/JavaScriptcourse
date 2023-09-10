a6ebd64d52c23de53b25a5b5f943e651e7c42ed9
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-10 21:14:58 +0530

    setInterval And setTimeOut

--- Special Topic/sync js/style.js
+++ Special Topic/sync js/style.js
@@ -2,12 +2,40 @@
 // // while (Date.now() - (start < 5000)) {
 // //   console.log("takking time");
 // // }
-console.log("start");
-const startTime = Date.now();
-const duration = 2000; // 5 seconds in milliseconds
+// console.log("start");
+// const startTime = Date.now();
+// const duration = 2000; // 5 seconds in milliseconds
 
-while (Date.now() - startTime < duration) {
-  // This loop will run for a maximum of 5 seconds
-}
+// while (Date.now() - startTime < duration) {
+//   // This loop will run for a maximum of 5 seconds
+// }
 
-console.log("end");
+// console.log("end");
+// console.log("Start");
+
+// let x = setTimeout(() => {
+//   console.log("timer one");
+// }, 2000);
+// clearTimeout(x);
+
+// console.log("end");
+
+//clearTimeOut
+// let x;
+// function a() {
+//   x = setTimeout(a, 2000);
+//   console.log("hello");
+//   clearTimeout(x);
+// }
+// a();
+//extra parameter
+// function greeting(name, mmsg) {
+//   console.log(name, mmsg);
+// }
+
+// setTimeout(greeting, 2000, "Abhishek", "hello hi");
+//setInterval
+// let count = 0;
+// let id = setInterval(() => {
+//   console.log("hello");
+// }, 1000);
