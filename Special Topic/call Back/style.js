3cd8d4c3aaa08f850ab9be4551a18c894938331a
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-11 20:05:19 +0530

    call back

---
+++ Special Topic/call Back/style.js
@@ -0,0 +1,41 @@
+// setTimeout(() => {
+//   console.log("1 second passed");
+//   setTimeout(() => {
+//     console.log("2 seconds passed");
+//     setTimeout(() => {
+//       console.log("3 second passed");
+//       setTimeout(() => {
+//         console.log("4 second passed");
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+//sync callback
+// function print(gretting) {
+//   console.log("start");
+//   gretting();
+//   console.log("end");
+// }
+
+// function gretting() {
+//   console.log("hello namaste");
+// }
+
+// print(gretting);
+
+//async
+
+function print(gretting) {
+  console.log("start");
+  gretting();
+  console.log("end");
+}
+
+function gretting() {
+  setTimeout(() => {
+    console.log("hello namaste");
+  }, 1000);
+}
+
+print(gretting);
