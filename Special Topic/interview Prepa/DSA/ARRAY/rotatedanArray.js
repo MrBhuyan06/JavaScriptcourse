a4b324baec9df5544e07a431be11ea869ac18713
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-11 14:26:17 +0530

    by n positon

--- Special Topic/interview Prepa/DSA/ARRAY/rotatedanArray.js
+++ Special Topic/interview Prepa/DSA/ARRAY/rotatedanArray.js
@@ -14,13 +14,16 @@
 
 // for nth postion
 let arr = [1, 2, 3, 4, 5, 7];
-function rotatingArray(arr) {
-  let temp = arr[arr.length - 1];
-  for (let i = arr.length - 1; i >= 1; i--) {
-    arr[i] = arr[i - 1];
+function rotatingArray(arr, n) {
+  for (let j = 0; j <= n - 1; j++) {
+    let temp = arr[arr.length - 1];
+
+    for (let i = arr.length - 1; i >= 1; i--) {
+      arr[i] = arr[i - 1];
+    }
+    arr[0] = temp;
   }
-  arr[0] = temp;
   return arr;
 }
-let res = rotatingArray(arr);
+let res = rotatingArray(arr, 2);
 console.log(res);
