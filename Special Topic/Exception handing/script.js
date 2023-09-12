d3ba4436623f3d6be9cae51a36f78dfcbe13a573
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-12 20:30:35 +0530

    added the async code example

---
+++ Special Topic/Exception handing/script.js
@@ -0,0 +1,22 @@
+// try {
+//   console.log("abhihsker");
+//   e, f[pmerf];
+//   console.log("ofjwofoh");
+// } catch (abs) {
+//   console.log(abs.message);
+//   console.log(abs.name);
+//   console.log(abs.stack);
+// }
+//try watch is sync inside it asunc code
+try {
+  setTimeout(() => {
+    // console.log("1000");
+    try {
+      blavla;
+    } catch (err) {
+      console.log(err);
+    }
+  }, 1000);
+} catch (err) {
+  console.log(err);
+}
