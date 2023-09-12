b427c5739b307b852e9f7360ab075fd45b009892
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-12 20:39:12 +0530

    added the trow error

--- Special Topic/Exception handing/script.js
+++ Special Topic/Exception handing/script.js
@@ -8,15 +8,30 @@
 //   console.log(abs.stack);
 // }
 //try watch is sync inside it asunc code
+// try {
+//   setTimeout(() => {
+//     // console.log("1000");
+//     try {
+//       blavla;
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }, 1000);
+// } catch (err) {
+//   console.log(err);
+// }
+
+//undefine err
 try {
-  setTimeout(() => {
-    // console.log("1000");
-    try {
-      blavla;
-    } catch (err) {
-      console.log(err);
-    }
-  }, 1000);
+  let data = {
+    name: "Abhi",
+    add: "bbsr",
+  };
+  //   let objData = JSON.parse(data);
+  if (!data.age) {
+    // console.log(objData);
+    throw new SyntaxError("someThink is went wrong");
+  }
 } catch (err) {
-  console.log(err);
+  console.log(err.name);
 }
